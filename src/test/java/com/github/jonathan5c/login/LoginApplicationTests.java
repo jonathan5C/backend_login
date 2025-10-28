a639dc758252c2e1815f69b40c9945762d7b0763
Jonathan Macedo Castro <jonathan.castro18@outlook.com>
2025-10-28 15:28:11 -0300

    chore: Create the project base

---
+++ src/test/java/com/github/jonathan5c/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jonathan5c.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
